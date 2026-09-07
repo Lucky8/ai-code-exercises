1e1cec4c770af62bf8c2103f6ae615b31e6fc515
Ltech Ndlovu <Lucky8@users.noreply.github.com>
2026-09-07 16:34:24 +0200

    save updates

--- use-cases/debug-limitations/javascript/merge_sort.js
+++ use-cases/debug-limitations/javascript/merge_sort.js
@@ -24,10 +24,10 @@ function merge(left, right) {
         }
     }
 
-    // Bug: Only one of these loops will execute
+    // Append any elements left after one half is exhausted.
     while (i < left.length) {
         result.push(left[i]);
-        j++; // Bug: incrementing j instead of i
+        i++;
     }
 
     while (j < right.length) {
